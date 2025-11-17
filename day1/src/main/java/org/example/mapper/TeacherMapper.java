4260e50d6d43022ed8d41420dc37e4064436fb98
Long_yi <2189172500@qq.com>
2025-11-17 20:41:09 +0800

    day05增加通过拼接sql完成的批量删除，批量更改方法

--- day1/src/main/java/org/example/mapper/TeacherMapper.java
+++ day1/src/main/java/org/example/mapper/TeacherMapper.java
@@ -1,17 +1,23 @@
 package org.example.mapper;
 
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 import org.example.unity.Teacher;
 import java.util.List;
 import java.util.Map;
 
 public interface TeacherMapper {
+    @Select("select  * from teacher")
     List<Teacher> selectAllTeachers();
-    int batchInsertTeachers(List<Teacher> teachers);
+    int deleteById(Integer id);
+    int updateById(Teacher teacher);
     int insertTeacherById(Teacher teacher);
+    int batchInsertTeachers(List<Teacher> teachers);
     int batchDelete(int[] ids);
-    int batchInsert();
     int deleteByCondition(Map<String, Object> condition);
     List<Teacher> selectByCondition(Map<String, Object> params);
-    double getJilinAverageSalary();
+    double getJilinAverageSalary(String province);
     int promoteSalesToManager();
 }
